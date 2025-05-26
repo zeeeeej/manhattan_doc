4aaf0a53a149023d23bc7a49282bb96e7f18f8ca
xpl <xiangpl@hadlinks.com>
2025-05-25 23:27:20 -0700

    exit .h

--- include/uart_parser.h
+++ include/uart_parser.h
@@ -51,10 +51,10 @@ extern "C" {
 
 #define bswap_16(A) (( ((uint16_t)(A) & 0xff00) >> 8) | (( (uint16_t)(A) & 0x00ff) << 8))  
  
-#define bswap_32(A) ((( (uint32_t)(A) & 0xff000000) >> 24) | \ 
-                          (( (uint32_t)(A) & 0x00ff0000) >> 8)  | \ 
-                          (( (uint32_t)(A) & 0x0000ff00) << 8)  | \ 
-                          (( (uint32_t)(A) & 0x000000ff) << 24)) 
+#define bswap_32(A) ((( (uint32_t)(A) & 0xff000000) >> 24) | \
+                          (( (uint32_t)(A) & 0x00ff0000) >> 8)  | \
+                          (( (uint32_t)(A) & 0x0000ff00) << 8)  | \
+                          (( (uint32_t)(A) & 0x000000ff) << 24))
 
 enum cmd_uart_state
 {
