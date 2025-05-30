01560db3526bbdfd8d127f808c9401d8c955e5d2
xiangpl <xiangpengle@qq.com>
2025-05-30 09:31:34 +0800

    0.0.6

---
+++ include/api.h
@@ -0,0 +1,20 @@
+#ifndef _API_H
+#define _API_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int api_get_pic_compress();
+
+int api_get_gs_angle();
+
+int api_get_gs_rotation();
+
+int api_get_max_pic_num();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
