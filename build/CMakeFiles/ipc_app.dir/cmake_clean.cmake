dfd9a0b395c1c4554ce48c6bab30433bb8a2202f
xiangpl <xiangpengle@qq.com>
2025-08-20 13:11:39 +0800

    0.0.6-debug-rk-uart-sendbyte

---
+++ build/CMakeFiles/ipc_app.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/xiangpengle/Documents/job/manhattan/存档/app_sdk_v0.0.6/bin/ipc_app"
+  "/Users/xiangpengle/Documents/job/manhattan/存档/app_sdk_v0.0.6/bin/ipc_app.pdb"
+  "CMakeFiles/ipc_app.dir/src/main.c.o"
+  "CMakeFiles/ipc_app.dir/src/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ipc_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
