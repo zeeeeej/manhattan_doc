36c4a437f678754d3be7707e9653c6abaabd59e2
xiangpl <xiangpengle@qq.com>
2025-08-21 14:36:48 +0800

    rk_uart_sendbyte in thread.

--- src/main.c
+++ src/main.c
@@ -112,24 +112,32 @@ static void *test_485_send(void *arg) {
 	return NULL;
 }
 
-static void my_cus_recv(uint8_t str)
-{
-	LOG_INFO("cus_recv %02X\n", str);
-	printf("收到%02x\n", str);
-	usleep(100);
-  
-    printf("开始发送\n");
+static pthread_t debug_uart_write;
 
+static void *send_uart(void *arg) {
     rs485_pwr_on();
     usleep(9000);
-	  int size = 200000;
-    for (size_t i = 0; i < size; ++i) {
+    int size = 200000;
+    rk_uart_sendbyte(0xaa);
+    rk_uart_sendbyte(0x5a);
+    for (size_t i = 0; i < size - 4; ++i) {
         rk_uart_sendbyte(0xfe);
         //usleep(0);
     }
+    rk_uart_sendbyte(0xaa);
+    rk_uart_sendbyte(0x5a);
     usleep(4000);
     rs485_pwr_off();
 
+    return NULL;
+}
+
+static void my_cus_recv(uint8_t str)
+{
+	LOG_INFO("cus_recv %02X\n", str);
+    printf("开始发送\n");
+    pthread_create(&debug_uart_write, NULL, send_uart, NULL);
+    pthread_join(debug_uart_write,NULL);
     printf("发送完毕\n");
 
 }
