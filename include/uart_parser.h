0def9575b6117c147a37f15c4d3d06ef9e073054
xiangpl <xiangpengle@qq.com>
2025-08-27 13:29:43 +0800

    fix bswap_16

--- include/uart_parser.h
+++ include/uart_parser.h
@@ -49,12 +49,12 @@ extern "C" {
 #define SUBSYSTEM_ATTR_DOOR_ANG 0x14
 
 
-#define bswap_16(A) (( ((uint16_t)(A) & 0xff00) >> 8) | (( (uint16_t)(A) & 0x00ff) << 8))  
+// #define bswap_16(A) (( ((uint16_t)(A) & 0xff00) >> 8) | (( (uint16_t)(A) & 0x00ff) << 8))  
  
-#define bswap_32(A) ((( (uint32_t)(A) & 0xff000000) >> 24) | \
-                          (( (uint32_t)(A) & 0x00ff0000) >> 8)  | \
-                          (( (uint32_t)(A) & 0x0000ff00) << 8)  | \
-                          (( (uint32_t)(A) & 0x000000ff) << 24)) 
+// #define bswap_32(A) ((( (uint32_t)(A) & 0xff000000) >> 24) | \
+//                           (( (uint32_t)(A) & 0x00ff0000) >> 8)  | \
+//                           (( (uint32_t)(A) & 0x0000ff00) << 8)  | \
+//                           (( (uint32_t)(A) & 0x000000ff) << 24)) 
 
 enum cmd_uart_state
 {
